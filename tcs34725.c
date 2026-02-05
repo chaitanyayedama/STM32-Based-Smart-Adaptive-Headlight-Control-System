0a4d6088082aedbe1911ddf98e52cfd3c3b8bd7e
chaitanyayedama <chaitanya.yedama@gmail.com>
2026-02-05 11:43:51 +0530

    Add files via upload

---
+++ tcs34725.c
@@ -0,0 +1,226 @@
+#include "tcs34725.h"
+#include "stm32f4xx.h"
+
+extern I2C_HandleTypeDef hi2c1;
+tcs34725IntegrationTime Int_Time= TCS34725_INTEGRATIONTIME_2_4MS;
+tcs34725Gain Gain= TCS34725_GAIN_1X;
+uint8_t _tcs34725Initialised = 0;
+
+void write8 (uint8_t reg, uint32_t value)
+{
+    uint8_t txBuffer[2];
+    txBuffer[0] = (TCS34725_COMMAND_BIT | reg);
+    txBuffer[1] = (value & 0xFF);
+    HAL_I2C_Master_Transmit(&hi2c1, TCS34725_ADDRESS, txBuffer, 2, 100);
+}
+
+/* Reads an 8 bit value over I2C */
+uint8_t read8(uint8_t reg)
+{
+    uint8_t buffer[1];
+    buffer[0] = (TCS34725_COMMAND_BIT | reg);
+    HAL_I2C_Master_Transmit(&hi2c1, TCS34725_ADDRESS, buffer, 1, 100);
+    HAL_I2C_Master_Receive(&hi2c1, TCS34725_ADDRESS, buffer, 1, 100);
+    return buffer[0];
+}
+
+/* Reads a 16 bit values over I2C */
+uint16_t read16(uint8_t reg)
+{
+  uint16_t ret;
+    uint8_t txBuffer[1],rxBuffer[2];
+    txBuffer[0] = (TCS34725_COMMAND_BIT | reg);
+    HAL_I2C_Master_Transmit(&hi2c1, TCS34725_ADDRESS, txBuffer, 1, 100);
+    HAL_I2C_Master_Receive(&hi2c1, TCS34725_ADDRESS, rxBuffer, 2, 100);
+    ret = rxBuffer[1];
+    ret <<= 8;
+    ret |= rxBuffer[0] & 0xFF;
+  return ret;
+}
+
+void enable(void)
+{
+  write8(TCS34725_ENABLE, TCS34725_ENABLE_PON);
+  HAL_Delay(3);
+  write8(TCS34725_ENABLE, TCS34725_ENABLE_PON | TCS34725_ENABLE_AEN);
+  HAL_Delay((256 - Int_Time)*12/4 + 1); //+1 for integer truncation
+}
+
+
+void disable(void)
+{
+  /* Turn the device off to save power */
+  uint8_t reg = 0;
+  reg = read8(TCS34725_ENABLE);
+  write8(TCS34725_ENABLE, reg & ~(TCS34725_ENABLE_PON | TCS34725_ENABLE_AEN));
+}
+
+void setIntegrationTime(uint8_t itime)
+{
+  if (_tcs34725Initialised == 0) tcs3272_init();
+  write8(TCS34725_ATIME, itime);
+}
+
+void setGain(uint8_t gain)
+{
+  if (_tcs34725Initialised == 0) tcs3272_init();
+  write8(TCS34725_CONTROL, gain);
+}
+
+void tcs3272_init(void)
+{
+  /* Make sure we're actually connected */
+
+  uint8_t readValue = read8(TCS34725_ID);
+  if ((readValue != 0x4d) &&(readValue != 0x44) && (readValue != 0x10)){
+    return;
+  }
+  _tcs34725Initialised = 1;
+  /* Set default integration time and gain */
+  setIntegrationTime(Int_Time);
+  setGain(Gain);
+  /* Note: by default, the device is in power down mode on bootup */
+  enable();
+}
+
+/* Get raw data */
+void getRawData (uint16_t *r, uint16_t *g, uint16_t *b, uint16_t *c)
+{
+  if (_tcs34725Initialised == 0) tcs3272_init();
+
+  *c = read16(TCS34725_CDATAL);
+  *r = read16(TCS34725_RDATAL);
+  *g = read16(TCS34725_GDATAL);
+  *b = read16(TCS34725_BDATAL);
+  /* Delay time is from page no 16/26 from the datasheet  (256 − ATIME)* 2.4ms */
+  HAL_Delay((256 - Int_Time)*12/4 + 1);
+}
+
+/* Get Red, Green and Blue color from Raw Data */
+void getRGB(uint16_t *R, uint16_t *G, uint16_t *B,uint16_t *C)
+{
+    uint16_t rawRed, rawGreen, rawBlue, rawClear;
+    getRawData(&rawRed, &rawGreen, &rawBlue, &rawClear);
+    if(rawClear == 0)
+    {
+      *R = 0;
+      *G = 0;
+      *B = 0;
+      *C = 0;
+    }
+    else
+    {
+      *R = (uint16_t)rawRed / rawClear * 255 ;
+      *G = (uint16_t)rawGreen / rawClear * 255 ;
+      *B = (uint16_t)rawBlue / rawClear * 255 ;
+      *C = (uint16_t)rawClear;
+    }
+}
+
+/*
+Converts the raw R/G/B values to color temperature in degrees Kelvin using the algorithm described in DN40 from Taos (now AMS).
+ *  @param  r
+ *          Red value
+ *  @param  g
+ *          Green value
+ *  @param  b
+ *          Blue value
+ *  @param  c
+ *          Clear channel value
+ *  @return Color temperature in degrees Kelvin
+ */
+
+uint16_t calculateColorTemperature(uint16_t r, uint16_t g, uint16_t b, uint16_t c) {
+  uint16_t r2, b2; /* RGB values minus IR component */
+  uint16_t sat;    /* Digital saturation level */
+  uint16_t ir;     /* Inferred IR content */
+
+  if (c == 0)
+    return 0;
+
+  /* Analog/Digital saturation:
+   *
+   * (a) As light becomes brighter, the clear channel will tend to
+   *     saturate first since R+G+B is approximately equal to C.
+   * (b) The TCS34725 accumulates 1024 counts per 2.4ms of integration
+   *     time, up to a maximum values of 65535. This means analog
+   *     saturation can occur up to an integration time of 153.6ms
+   *     (64*2.4ms=153.6ms).
+   * (c) If the integration time is > 153.6ms, digital saturation will
+   *     occur before analog saturation. Digital saturation occurs when
+   *     the count reaches 65535.
+   */
+  if ((256 - Int_Time) > 63) {
+    /* Track digital saturation */
+    sat = 65535;
+  } else {
+    /* Track analog saturation */
+    sat = 1024 * (256 - Int_Time);
+  }
+
+  /* Ripple rejection:
+   *
+   * (a) An integration time of 50ms or multiples of 50ms are required to
+   *     reject both 50Hz and 60Hz ripple.
+   * (b) If an integration time faster than 50ms is required, you may need
+   *     to average a number of samples over a 50ms period to reject ripple
+   *     from fluorescent and incandescent light sources.
+   *
+   * Ripple saturation notes:
+   *
+   * (a) If there is ripple in the received signal, the value read from C
+   *     will be less than the max, but still have some effects of being
+   *     saturated. This means that you can be below the 'sat' value, but
+   *     still be saturating. At integration times >150ms this can be
+   *     ignored, but <= 150ms you should calculate the 75% saturation
+   *     level to avoid this problem.
+   */
+  if ((256 - Int_Time) <= 63) {
+    /* Adjust sat to 75% to avoid analog saturation if atime < 153.6ms */
+    sat -= sat / 4;
+  }
+
+  /* Check for saturation and mark the sample as invalid if true */
+  if (c >= sat) {
+    return 0;
+  }
+
+  /* AMS RGB sensors have no IR channel, so the IR content must be */
+  /* calculated indirectly. */
+  ir = (r + g + b > c) ? (r + g + b - c) / 2 : 0;
+
+  /* Remove the IR component from the raw RGB values */
+  r2 = r - ir;
+  b2 = b - ir;
+
+  if (r2 == 0) {
+    return 0;
+  }
+
+  /* A simple method of measuring color temp is to use the ratio of blue */
+  /* to red light, taking IR cancellation into account. */
+  uint16_t cct = (3810 * (uint32_t)b2) / (uint32_t)r2 + 1391;
+              /*Color temp coefficient*/  /*Color temp offset*/
+
+  return cct;
+}
+
+/*
+ *  @brief  Converts the raw R/G/B values to lux
+ *  @param  r
+ *          Red value
+ *  @param  g
+ *          Green value
+ *  @param  b
+ *          Blue value
+ *  @return Lux value
+ */
+uint16_t calculateLux(uint16_t r, uint16_t g, uint16_t b) {
+  float illuminance;
+
+  /* This only uses RGB ... how can we integrate clear or calculate lux */
+  /* based exclusively on clear since this might be more reliable?      */
+  illuminance = (-0.32466F * r) + (1.57837F * g) + (-0.73191F * b);
+
+  return (uint16_t)illuminance;
+}
